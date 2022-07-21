1253f61650bf7796735342b9820d520ac3b1d4fe
sou <ocelot2828@gmail.com>
2022-07-21 15:32:27 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
 </script>
-
+<!-- test -->
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
